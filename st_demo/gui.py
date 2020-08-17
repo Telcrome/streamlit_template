46604af8a10a38a72f15b17b638986958dd421b8
Telcrome <raphaelschaefer1@outlook.com>
2020-08-17 19:08:54 +0200

    Add st cache demo

--- st_demo/gui.py
+++ st_demo/gui.py
@@ -1,7 +1,22 @@
 import numpy as np
 import streamlit as st
+import skimage.data as skdata
+import time
+
+img = skdata.astronaut()
 
 arr = np.random.random(5)
+
+st.image(img)
+
+@st.cache
+def get_data() -> np.ndarray:
+    time.sleep(5)
+    return np.random.random(5)
+
+st.write(get_data())
+
+st.write('changed something')
 
 st.write(arr)
 
